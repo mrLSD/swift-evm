31e20c1b4829bb94f5cc9bfa24001775fb723354
Evgeny Ukhanov <evgeny@aurora.dev>
2024-09-15 03:56:15 +0200

    State functions, and StateTests

--- Sources/Interpreter/Stack.swift
+++ Sources/Interpreter/Stack.swift
@@ -9,21 +9,28 @@ public struct Stack {
     /// Stacke length
     public var length: Int { self.data.count }
 
+    /// Init Machine Stack with specific data limit
     init(limit: Int) {
         self.limit = limit
     }
 
     /// Push `U256` value to the Stack. Return error if it's reach limit.
-    mutating func push(value: U256) -> Machine.ExitError? {
+    ///
+    /// - Parameter value: `U256` value that will be pushed to Stacl
+    /// - Returns: A `Result` containing the `VOid` value if successful, or an `ExitError.StackOverflow` if an error occurs.
+    @inline(__always)
+    mutating func push(value: U256) -> Result<Void, Machine.ExitError> {
         // Check stack overflow
         if self.data.count + 1 > self.limit {
-            return .StackOverflow
+            return .failure(.StackOverflow)
         }
-        self.data.append(value)
-        return nil
+        return .success(self.data.append(value))
     }
 
     /// Pop `U256` value from the Stack
+    ///
+    /// - Returns: A `Result` containing the `H256` value if successful, or an `ExitError.StackUnderflow` if stack is empty.
+    @inline(__always)
     mutating func pop() -> Result<U256, Machine.ExitError> {
         guard let value = self.data.popLast() else {
             // Return error, if stack is empty
@@ -33,7 +40,10 @@ public struct Stack {
     }
 
     /// Pop `H256` value from the Stack
-    mutating func pop() -> Result<H256, Machine.ExitError> {
+    ///
+    /// - Returns: A `Result` containing the `H256` value if successful, or an `ExitError.StackUnderflow` if stack is empty.
+    @inline(__always)
+    mutating func popH256() -> Result<H256, Machine.ExitError> {
         guard let value = self.data.popLast() else {
             // Return error, if stack is empty
             return .failure(.StackUnderflow)
@@ -47,6 +57,7 @@ public struct Stack {
     ///
     /// - Parameter indexFromTop: The index from the top of the stack.
     /// - Returns: A `Result` containing the `U256` value if successful, or an `ExitError` if an error occurs.
+    @inline(__always)
     func peek(indexFromTop: Int) -> Result<U256, Machine.ExitError> {
         // Ensure the index is non-negative and within the bounds of the Stack data array
         guard indexFromTop >= 0, indexFromTop < self.data.count else {
@@ -63,6 +74,7 @@ public struct Stack {
     ///
     /// - Parameter indexFromTop: The index from the top of the stack.
     /// - Returns: A `Result` containing the `H256` value if successful, or an `ExitError` if an error occurs.
+    @inline(__always)
     func peekH256(indexFromTop: Int) -> Result<H256, Machine.ExitError> {
         self.peek(indexFromTop: indexFromTop).map { u256 in
             H256(from: u256.toBigEndian)
@@ -74,17 +86,36 @@ public struct Stack {
     ///
     /// - Parameter indexFromTop: The index from the top of the stack.
     /// - Returns: A `Result` containing the `Int` value if successful, or an `ExitError` if an error occurs.
-    func peek(indexFromTop: Int) -> Result<Int, Machine.ExitError> {
-        switch peek(indexFromTop: indexFromTop) {
+    @inline(__always)
+    func peekUInt(indexFromTop: Int) -> Result<UInt, Machine.ExitError> {
+        switch self.peek(indexFromTop: indexFromTop) {
         case .success(let u256):
-            U256.getMax
-            let x = u256
-            guard let intValue = u256.asUsable() else {
+            guard let intValue = u256.getUInt else {
                 return .failure(.OutOfGas)
             }
             return .success(intValue)
         case .failure(let error):
             return .failure(error)
+        }
+    }
+
+    /// Sets a value at the specified index from the top of the stack.
+    ///
+    /// The top of the stack is considered at index `0`. If the provided index is out of bounds,
+    /// the function returns a failure with `ExitError.stackUnderflow`.
+    ///
+    /// - Parameters:
+    ///   - indexFromTop: The zero-based index from the top of the stack.
+    ///   - value: The `U256` value to set at the specified index.
+    /// - Returns: A `Result` indicating success (`Void`) or failure (`ExitError`).
+    @inline(__always)
+    mutating func set(indexFromTop: Int, value: U256) -> Result<Void, Machine.ExitError> {
+        if self.data.count > indexFromTop, indexFromTop >= 0 {
+            let index = self.data.count - indexFromTop - 1
+            self.data[index] = value
+            return .success(())
+        } else {
+            return .failure(.StackUnderflow)
         }
     }
 }
