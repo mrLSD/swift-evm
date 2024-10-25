0b790307a74405af415d18dd153fb1dc501dbf91
Evgeny Ukhanov <evgeny@aurora.dev>
2024-10-25 12:27:21 +0200

    Added AND and OR tests

--- Tests/InterpreterTests/InstructionsTests/Arithmetic/AddTests.swift
+++ Tests/InterpreterTests/InstructionsTests/Arithmetic/AddTests.swift
@@ -65,23 +65,23 @@ final class InstructionAddSpec: QuickSpec {
                 expect(m.stack.length).to(equal(0))
                 expect(m.gas.remaining).to(equal(2))
             }
-        }
 
-        it("check stack") {
-            var m = Self.machine
-            m.evalLoop()
-            expect(m.machineStatus).to(equal(.Exit(.Error(.StackUnderflow))))
+            it("check stack") {
+                var m = Self.machine
+                m.evalLoop()
+                expect(m.machineStatus).to(equal(.Exit(.Error(.StackUnderflow))))
 
-            var m1 = Self.machine
-            let _ = m1.stack.push(value: U256(from: 5))
-            m1.evalLoop()
-            expect(m1.machineStatus).to(equal(.Exit(.Error(.StackUnderflow))))
+                var m1 = Self.machine
+                let _ = m1.stack.push(value: U256(from: 5))
+                m1.evalLoop()
+                expect(m1.machineStatus).to(equal(.Exit(.Error(.StackUnderflow))))
 
-            var m2 = Self.machine
-            let _ = m2.stack.push(value: U256(from: 2))
-            let _ = m2.stack.push(value: U256(from: 2))
-            m2.evalLoop()
-            expect(m2.machineStatus).to(equal(.Exit(.Success(.Stop))))
+                var m2 = Self.machine
+                let _ = m2.stack.push(value: U256(from: 2))
+                let _ = m2.stack.push(value: U256(from: 2))
+                m2.evalLoop()
+                expect(m2.machineStatus).to(equal(.Exit(.Success(.Stop))))
+            }
         }
     }
 }
