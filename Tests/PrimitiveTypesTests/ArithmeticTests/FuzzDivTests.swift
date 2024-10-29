cdb8557eec3aa193a664ee48334b6a5f14334cd3
Evgeny Ukhanov <evgeny@aurora.dev>
2024-10-29 23:39:53 +0100

    Change fuzz for CI

--- Tests/PrimitiveTypesTests/ArithmeticTests/FuzzDivTests.swift
+++ Tests/PrimitiveTypesTests/ArithmeticTests/FuzzDivTests.swift
@@ -14,25 +14,27 @@ final class FuzzDivRemSpec: QuickSpec {
             }
 
             it("run fuzz for divRem") {
-                var index = 0
-                // while index < 10 {
-                index += 1
+                /*
+                 var index = 0
+                 while index < 10 {
+                     index += 1
 
-                let val1 = UInt128.random(in: 2 ... UInt128.max)
-                let (hi1, lo1) = splitUInt128(val1)
-                let val2 = UInt128.random(in: 2 ... UInt128.max)
-                let (hi2, lo2) = splitUInt128(val2)
-                let a = U128(from: [lo1, hi1])
-                let b = U128(from: [lo2, hi2])
-//                let (quotient, remainder) = a.divRem(divisor: b)
-//
-//                let div_val = val1 / val2
-//                let rem_val = val1 % val2
-//                let (div_hi, div_lo) = splitUInt128(div_val)
-//                let (rem_hi, rem_lo) = splitUInt128(rem_val)
-//                expect(quotient.BYTES).to(equal([div_lo, div_hi]))
-//                expect(remainder.BYTES).to(equal([rem_lo, rem_hi]))
-                // }
+                     let val1 = UInt128.random(in: 2 ... UInt128.max)
+                     let (hi1, lo1) = splitUInt128(val1)
+                     let val2 = UInt128.random(in: 2 ... UInt128.max)
+                     let (hi2, lo2) = splitUInt128(val2)
+                     let a = U128(from: [lo1, hi1])
+                     let b = U128(from: [lo2, hi2])
+                     let (quotient, remainder) = a.divRem(divisor: b)
+
+                     let div_val = val1 / val2
+                     let rem_val = val1 % val2
+                     let (div_hi, div_lo) = splitUInt128(div_val)
+                     let (rem_hi, rem_lo) = splitUInt128(rem_val)
+                     expect(quotient.BYTES).to(equal([div_lo, div_hi]))
+                     expect(remainder.BYTES).to(equal([rem_lo, rem_hi]))
+                 }
+                     */
             }
         }
     }
