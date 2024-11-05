e6fbcb8d0474117f08130060c575ca4f3a28c9d7
Evgeny Ukhanov <evgeny@aurora.dev>
2024-11-05 11:19:37 +0100

    Signextend instruction tests - extended

--- Tests/InterpreterTests/InstructionsTests/SignextendTests.swift
+++ Tests/InterpreterTests/InstructionsTests/SignextendTests.swift
@@ -14,7 +14,7 @@ final class InstructionSignextendSpec: QuickSpec {
         describe("Instruction Signextend") {
             let handler = Handler()
 
-            it("40 sign 10") {
+            it("40 sign") {
                 var m = Machine(data: [], code: [Opcode.SIGNEXTEND.rawValue], gasLimit: 10, handler: handler)
 
                 let _ = m.stack.push(value: U256(from: 10))
@@ -31,10 +31,10 @@ final class InstructionSignextendSpec: QuickSpec {
                 expect(m.gas.remaining).to(equal(5))
             }
 
-            it("30 sign 100") {
+            it("5 sign") {
                 var m = Machine(data: [], code: [Opcode.SIGNEXTEND.rawValue], gasLimit: 10, handler: handler)
 
-                let _ = m.stack.push(value: U256(from: [0xFA, 0xFC, 0, 0]))
+                let _ = m.stack.push(value: U256(from: [3, 4, 5, 6]))
                 let _ = m.stack.push(value: U256(from: 5))
 
                 m.evalLoop()
@@ -42,7 +42,58 @@ final class InstructionSignextendSpec: QuickSpec {
 
                 expect(m.machineStatus).to(equal(.Exit(.Success(.Stop))))
                 expect(result).to(beSuccess { value in
-                    expect(value).to(equal(U256(from: 0xFA)))
+                    expect(value).to(equal(U256(from: [3, 0, 0, 0])))
+                })
+                expect(m.stack.length).to(equal(0))
+                expect(m.gas.remaining).to(equal(5))
+            }
+
+            it("10 sign") {
+                var m = Machine(data: [], code: [Opcode.SIGNEXTEND.rawValue], gasLimit: 10, handler: handler)
+
+                let _ = m.stack.push(value: U256(from: [3, 4, 5, 6]))
+                let _ = m.stack.push(value: U256(from: 10))
+
+                m.evalLoop()
+                let result = m.stack.pop()
+
+                expect(m.machineStatus).to(equal(.Exit(.Success(.Stop))))
+                expect(result).to(beSuccess { value in
+                    expect(value).to(equal(U256(from: [3, 4, 0, 0])))
+                })
+                expect(m.stack.length).to(equal(0))
+                expect(m.gas.remaining).to(equal(5))
+            }
+
+            it("20 sign") {
+                var m = Machine(data: [], code: [Opcode.SIGNEXTEND.rawValue], gasLimit: 10, handler: handler)
+
+                let _ = m.stack.push(value: U256(from: [3, 4, 5, 6]))
+                let _ = m.stack.push(value: U256(from: 20))
+
+                m.evalLoop()
+                let result = m.stack.pop()
+
+                expect(m.machineStatus).to(equal(.Exit(.Success(.Stop))))
+                expect(result).to(beSuccess { value in
+                    expect(value).to(equal(U256(from: [3, 4, 5, 0])))
+                })
+                expect(m.stack.length).to(equal(0))
+                expect(m.gas.remaining).to(equal(5))
+            }
+
+            it("30 sign") {
+                var m = Machine(data: [], code: [Opcode.SIGNEXTEND.rawValue], gasLimit: 10, handler: handler)
+
+                let _ = m.stack.push(value: U256(from: [3, 4, 5, 6]))
+                let _ = m.stack.push(value: U256(from: 30))
+
+                m.evalLoop()
+                let result = m.stack.pop()
+
+                expect(m.machineStatus).to(equal(.Exit(.Success(.Stop))))
+                expect(result).to(beSuccess { value in
+                    expect(value).to(equal(U256(from: [3, 4, 5, 6])))
                 })
                 expect(m.stack.length).to(equal(0))
                 expect(m.gas.remaining).to(equal(5))
