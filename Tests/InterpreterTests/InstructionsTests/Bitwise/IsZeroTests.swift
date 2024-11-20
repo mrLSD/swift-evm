b497c7d0affa50d6894bfb67c035325f9fa326e7
Evgeny Ukhanov <evgeny@aurora.dev>
2024-11-20 02:24:00 +0100

    Added tests: BYTES, ISZERO

--- Tests/InterpreterTests/InstructionsTests/Bitwise/IsZeroTests.swift
+++ Tests/InterpreterTests/InstructionsTests/Bitwise/IsZeroTests.swift
@@ -11,6 +11,47 @@ final class InstructionIsZeroSpec: QuickSpec {
 
     override class func spec() {
         describe("Instruction ISZERO") {
+            it("isZero true") {
+                var m = Self.machine
+
+                let _ = m.stack.push(value: U256.ZERO)
+                m.evalLoop()
+                let result = m.stack.pop()
+
+                expect(m.machineStatus).to(equal(.Exit(.Success(.Stop))))
+                expect(result).to(beSuccess { value in
+                    expect(value).to(equal(U256(from: 1)))
+                })
+                expect(m.stack.length).to(equal(0))
+                expect(m.gas.remaining).to(equal(7))
+            }
+
+            it("isZero false") {
+                var m = Self.machine
+
+                let _ = m.stack.push(value: U256(from: 5))
+                m.evalLoop()
+                let result = m.stack.pop()
+
+                expect(m.machineStatus).to(equal(.Exit(.Success(.Stop))))
+                expect(result).to(beSuccess { value in
+                    expect(value).to(equal(U256(from: 0)))
+                })
+                expect(m.stack.length).to(equal(0))
+                expect(m.gas.remaining).to(equal(7))
+            }
+
+            it("with OutOfGas result") {
+                var m = Self.machineLowGas
+
+                let _ = m.stack.push(value: U256(from: 1))
+                m.evalLoop()
+
+                expect(m.machineStatus).to(equal(.Exit(.Error(.OutOfGas))))
+                expect(m.stack.length).to(equal(0))
+                expect(m.gas.remaining).to(equal(2))
+            }
+
             it("check stack") {
                 var m = Self.machine
                 m.evalLoop()
