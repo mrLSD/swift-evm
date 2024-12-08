56c2300db002ddf48c170f9218287dc7bcc8c821
Evgeny Ukhanov <evgeny@aurora.dev>
2024-12-09 00:11:56 +0100

    Extended SWAP tests

--- Tests/InterpreterTests/InstructionsTests/Stack/SwapTests.swift
+++ Tests/InterpreterTests/InstructionsTests/Stack/SwapTests.swift
@@ -9,8 +9,8 @@ final class InstructionSwapSpec: QuickSpec {
 
     override class func spec() {
         describe("Instruction SWAP") {
-            it("correct swap") {
-                for n in 0 ... UInt8(2) {
+            it("correct swap for SWAP1..SWAP16") {
+                for n in 0 ... UInt8(15) {
                     var m = TestMachine.machine(rawCode: [Opcode.SWAP1.rawValue + n], gasLimit: 10)
 
                     for i in (1 ... UInt64(n + 2)).reversed() {
@@ -19,15 +19,15 @@ final class InstructionSwapSpec: QuickSpec {
 
                     m.evalLoop()
                     let val1 = m.stack.peek(indexFromTop: 0)
-                    let val2 = m.stack.peek(indexFromTop: Int(n))
+                    let val2 = m.stack.peek(indexFromTop: Int(n) + 1)
 
                     expect(m.machineStatus).to(equal(.Exit(.Success(.Stop))))
                     expect(val1).to(beSuccess { value in
                         expect(value).to(equal(U256(from: UInt64(n) + 2)))
                     })
-//                    expect(val2).to(beSuccess { value in
-//                        expect(value).to(equal(U256(from: 1)))
-//                    })
+                    expect(val2).to(beSuccess { value in
+                        expect(value).to(equal(U256(from: 1)))
+                    })
 
                     expect(m.stack.length).to(equal(Int(n) + 2))
                     expect(m.gas.remaining).to(equal(7))
@@ -59,15 +59,15 @@ final class InstructionSwapSpec: QuickSpec {
         }
 
         it("check stack underflow for SWAP1..SWAP16") {
-            for n in 1 ... UInt8(15) {
+            for n in 0 ... UInt8(15) {
                 var m = TestMachine.machine(rawCode: [Opcode.SWAP1.rawValue + n], gasLimit: 10)
-                for i in (1 ... UInt64(n)).reversed() {
+                for i in (1 ... UInt64(n + 1)).reversed() {
                     let _ = m.stack.push(value: U256(from: i))
                 }
 
                 m.evalLoop()
                 expect(m.machineStatus).to(equal(.Exit(.Error(.StackUnderflow))))
-                expect(m.stack.length).to(equal(Int(n)))
+                expect(m.stack.length).to(equal(Int(n) + 1))
                 expect(m.gas.remaining).to(equal(10))
             }
         }
